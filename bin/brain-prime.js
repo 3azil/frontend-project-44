6d5a2eb20a3746b7b56b2a8d5ac1759e8c386be2
Bazil <studentvek21@mail.ru>
2024-03-07 01:58:10 +0300

    9step

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+// bin/brain-prime.js
+import { primeGame } from "../src/game/prime.js";
+primeGame();
---
+++ src/game/prime.js
@@ -0,0 +1,19 @@
+import runGame from "../index.js";
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const getGameRound = () => {
+  const question = Math.floor(Math.random() * 100) + 1;
+  const correctAnswer = isPrime(question) ? "yes" : "no";
+  return [String(question), correctAnswer];
+};
+
+const gameDescription =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
+export const primeGame = () => runGame(gameDescription, getGameRound);
